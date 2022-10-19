cc5ce6efd27abf748b2c99a91cf4817028a5c916
chad hinds <chadrickj8@gmail.com>
2022-10-19 15:28:15 -0400

    Adjust comment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const element = document.querySelector('.btn');
 
-// When button element var gets clicked, the target el gets transition styles applied
+// When button element var gets clicked, the target el gets transition styles applied.
 element.onclick = () => {
     console.log(document.querySelector('button'))
     document.querySelector('.box').className = 'transition'
